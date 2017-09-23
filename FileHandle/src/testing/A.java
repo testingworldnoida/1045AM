f5d9d0746172f175f344ab2e1e508018185d7072
testingworldnoida <testingworldglobal@gmail.com>
2017-09-23 11:16:07 +0530

    Task_Id=1234

---
+++ FileHandle/src/testing/A.java
@@ -0,0 +1,24 @@
+package testing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class A {
+
+
+	
+	public static void main(String[] args) throws IOException {
+
+       B o = new B();     
+       B.a=10;    
+       System.out.println(B.a);
+		
+	}
+	
+	
+}
---
+++ FileHandle/src/testing/B.java
@@ -0,0 +1,22 @@
+package testing;
+
+public class B {
+	
+	static int a;
+
+
+   static
+   {
+	   System.out.println("Static Block");
+	   
+   }
+	
+   
+   public B()
+   {
+	   System.out.println("Cons");
+   }
+   
+ 
+
+}
